84e7356d600c686c103d5475d3fd7fd48cac9d25
Mikpol21 <mikolajpolinski21@gmail.com>
2022-11-18 10:35:01 +0000

    Refactoring

--- scala/SATRunner.scala
+++ scala/SATRunner.scala
@@ -3,13 +3,14 @@ object SATRunner {
     def main(args: Array[String]): Unit = {
         val solver = SATSolver(args(0))
         val r: Float = args(1).toFloat
-        val testInput = List((10, 1000), (100, 1000), (1000, 1000), (10000, 100), (100000, 100))
+        val testInput = List((10, 1000), (100, 1000), (1000, 1000), (10000, 100))//, (100000, 10))
+        val k: Int = if(args.length > 2) args(2).toInt else 3
         //val testInput = List((10, 1))
         for((n, testCases) <- testInput) {
             var satisfied = 0
             var numberOfZeroes = 0;
             for(_ <- 0 until testCases){
-                val cnf = CNFGenerator.generate(n, (r*n.toFloat).toInt, 3)
+                val cnf = CNFGenerator.generate(n, (r*n.toFloat).toInt, k)
                 if(solver.solve(cnf))
                     satisfied += 1
                 else
